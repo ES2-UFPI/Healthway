7259b865f91e84382d5ff2a385fbbbcbe307247c
Kaua Marques <mkaua1011@gmail.com>
2024-11-15 11:13:45 -0300

    Teste para primeiro commit

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -10,54 +8,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class CounterScreen extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _CounterScreenState createState() => _CounterScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String message = "Loading...";
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    fetchMessage();
-  }
-
-  Future<void> fetchMessage() async {
-    final apiUrl = const String.fromEnvironment('API_URL', defaultValue: 'http://localhost:3000/');
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        setState(() {
-          message = data['message'];
-        });
-      } else {
-        setState(() {
-          message = "Failed to load message";
-        });
-      }
-    } catch (e) {
-      setState(() {
-        message = "Error: $e";
-      });
-    }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter + Express API'),
+        title: Text('Contador Flutter'),
       ),
       body: Center(
-        child: Text(message),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Você pressionou o botão esta quantidade de vezes:',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Incrementar',
+        child: Icon(Icons.add),
       ),
     );
   }
