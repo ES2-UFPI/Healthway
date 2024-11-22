a94cbec726c68f4b7467616471d057eb38aa972b
lucasherlon <lucasherlondsmc@gmail.com>
2024-11-22 11:46:48 -0300

    adicionando controllers e rotas para pacientes #18

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const nutricionistaRoutes = require('./routes/nutricionistaRoutes');
+const pacienteRoutes = require('./routes/pacienteRoutes');
 
 app.use(express.json());
-app.use('/api/nutricionista', nutricionistaRoutes);
+app.use('/api/nutricionistas', nutricionistaRoutes);
+app.use('/api/pacientes', pacienteRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
